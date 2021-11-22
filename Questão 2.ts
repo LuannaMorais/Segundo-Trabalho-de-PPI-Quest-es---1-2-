883ed4881e61eb31f9c2898ef946db988f7489c2
LuannaMorais <luanna.morais@escolar.ifrn.edu.br>
2021-11-22 11:46:59 -0300

    1 file modified

--- Questão 2.ts
+++ Questão 2.ts
@@ -1,4 +1,12 @@
 let data = new Date();
 let dia = data.getDay();
-let dias = ['Segunda feira', 'Terça Feira ', 'Quarta feira','Quinta feira', 'Sexta feira', 'Sábado' , 'Domingo'];
+let dias = [
+  'Segunda feira',
+  'Terça Feira ',
+  'Quarta feira',
+  'Quinta feira',
+  'Sexta feira',
+  'Sábado',
+  'Domingo',
+];
 console.log(dias[data.getDay()]);
